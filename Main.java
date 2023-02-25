38dc922227ab8f64abd44258ab883e8185bb92c7
damilola oyeyipo <oyeyipo45@gmail.com>
2023-02-25 01:17:52 +0100

    update repo with primitives

--- Main.java
+++ Main.java
@@ -1,37 +1,88 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * main
  */
-//import java.util.Scanner;
 
 class Main {
-   static int age = 12;
+    static int age = 12;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("What is the first number: ");
-        Double number1 = scanner.nextDouble();
+        // ARRAY LIST
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
 
-        System.out.print("What is the second number: ");
-        Double number2 = scanner.nextDouble();
-        scanner.nextLine();
 
-        System.out.print("What operation to take: ");
-        String operation = scanner.nextLine();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(44);
 
-        switch (operation) {
-            case "sum":  
-                System.out.printf("%f + %f = %f", number1, number2, number1 + number2);
-;
-                break;
+        numbers.remove(Integer.valueOf(44));
+
+
+        System.out.println(numbers.get(2));
+
+
+        // LOOPS
+        // int[] array = new int[10];
+        // for (int i = 0; i < array.length; i++) {
+        //     System.out.println(i);
+        // }
+
+        // int numbers[] = { 1, 2, 3, 4, 5 };
+        // int sum = 0;
+        // for (int number : numbers) {
+        //     sum += number;
+        //     System.out.println(sum);
+        // }
+
+ 
+        // COPY ARRAY
+        // char arr[] = { 'e', 'o', 'a', 'i', 'u' };
+        // int numbers[] = { 1, 2, 3, 4, 5 };
         
-            default:
-                break;
-        }
-  
+        // int copyNumbers[] = Arrays.copyOf(numbers, numbers.length);
+        // System.out.println(Arrays.equals(numbers, copyNumbers));
+
+ 
+
+        // SEARCHING FOR CHARACTER
+        // char key = 'o';
+        // int startIndex = 1;
+        // int endIndex = 4;
+        // Arrays.sort(arr);
+        // // you can only search characters from sorted arr and returns char index
+        // int index = Arrays.binarySearch(arr, key);
+        // int searchindex = Arrays.binarySearch(arr, startIndex, endIndex, key);
+
+        // System.out.println(Arrays.toString(arr));
+        // System.out.println(index);
+        // System.out.println(searchindex);
 
 
-        scanner.close();
+        // TAKING SYSTEM INP UTS
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.print("What is the first number: ");
+        // Double number1 = scanner.nextDouble();
+
+        // System.out.print("What is the second number: ");
+        // Double number2 = scanner.nextDouble();
+        // scanner.nextLine();
+
+        // System.out.print("What operation to take: ");
+        // String operation = scanner.nextLine();
+
+        // switch (operation) {
+        //     case "sum":
+        //         System.out.printf("%f + %f = %f", number1, number2, number1 + number2);
+        //         break;
+        //     default:
+        //         break;
+        // }
+
+        // scanner.close();
     }
 }
