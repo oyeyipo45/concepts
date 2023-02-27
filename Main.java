030df872830393840cac5861ef2e8974369dd28f
damilola oyeyipo <oyeyipo45@gmail.com>
2023-02-27 23:39:53 +0100

    Create user

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -7,82 +9,14 @@ import java.util.Scanner;
  */
 
 class Main {
-    static int age = 12;
-
     public static void main(String[] args) {
+        User user = new User();
 
-        // ARRAY LIST
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        user.name = "Damilola Oyeyipo";
+        user.birthDay = LocalDate.parse("1993-04-05");
 
+        System.out.printf("%s, was born in %s and is not %d years old", user.name, user.birthDay.toString(),
+                user.age());
 
-        numbers.add(3);
-        numbers.add(2);
-        numbers.add(44);
-
-        numbers.remove(Integer.valueOf(44));
-
-
-        System.out.println(numbers.get(2));
-
-
-        // LOOPS
-        // int[] array = new int[10];
-        // for (int i = 0; i < array.length; i++) {
-        //     System.out.println(i);
-        // }
-
-        // int numbers[] = { 1, 2, 3, 4, 5 };
-        // int sum = 0;
-        // for (int number : numbers) {
-        //     sum += number;
-        //     System.out.println(sum);
-        // }
-
- 
-        // COPY ARRAY
-        // char arr[] = { 'e', 'o', 'a', 'i', 'u' };
-        // int numbers[] = { 1, 2, 3, 4, 5 };
-        
-        // int copyNumbers[] = Arrays.copyOf(numbers, numbers.length);
-        // System.out.println(Arrays.equals(numbers, copyNumbers));
-
- 
-
-        // SEARCHING FOR CHARACTER
-        // char key = 'o';
-        // int startIndex = 1;
-        // int endIndex = 4;
-        // Arrays.sort(arr);
-        // // you can only search characters from sorted arr and returns char index
-        // int index = Arrays.binarySearch(arr, key);
-        // int searchindex = Arrays.binarySearch(arr, startIndex, endIndex, key);
-
-        // System.out.println(Arrays.toString(arr));
-        // System.out.println(index);
-        // System.out.println(searchindex);
-
-
-        // TAKING SYSTEM INP UTS
-        // Scanner scanner = new Scanner(System.in);
-
-        // System.out.print("What is the first number: ");
-        // Double number1 = scanner.nextDouble();
-
-        // System.out.print("What is the second number: ");
-        // Double number2 = scanner.nextDouble();
-        // scanner.nextLine();
-
-        // System.out.print("What operation to take: ");
-        // String operation = scanner.nextLine();
-
-        // switch (operation) {
-        //     case "sum":
-        //         System.out.printf("%f + %f = %f", number1, number2, number1 + number2);
-        //         break;
-        //     default:
-        //         break;
-        // }
-
-        // scanner.close();
     }
 }
---
+++ User.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class User {
+    public String name;
+    public LocalDate birthDay;
+
+    public int age() {
+        Period age = Period.between(this.birthDay, LocalDate.now());
+
+        return age.getYears();
+    }
+}
