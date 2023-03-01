fd95e00958e0d08b151d8396da0ff3dddcb9d1cf
Damilola Oyeyipo <oyeyipo45@gmail.com>
2023-03-01 14:56:46 +0100

    Create system inputs

--- src/Inputs.java
+++ src/Inputs.java
@@ -6,9 +6,7 @@ public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("What is your phone number");
-
         int phone = input.nextInt();
-
         System.out.println("This is your phone number " + phone);
     }
 }
